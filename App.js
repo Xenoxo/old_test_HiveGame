95c263528367406cec8f8a97ed14331b497587f5
Xenoxo <timwyc@gmail.com>
2018-05-07 21:05:52 +0200

    First commit. Set up scaffolding.

---
+++ App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <View style={{width:50, height:50, backgroundColor:'#cd5244'}}>
+        </View>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
