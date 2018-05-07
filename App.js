686bc87b279870cc665f1538b7a08086dcec0321
Xenoxo <timwyc@gmail.com>
2018-05-07 21:40:54 +0200

    Create button and basic state change on click

--- App.js
+++ App.js
@@ -1,11 +1,29 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
 
 export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      color:"#841584",
+    }
+  }
+  
+
+  onPressLearnMore = () => {
+    this.setState({color:"#cd5422"});
+  }
+
   render() {
     return (
       <View style={styles.container}>
         <View style={{width:50, height:50, backgroundColor:'#cd5244'}}>
+        <Button
+          onPress={this.onPressLearnMore}
+          title="Learn More"
+          color={this.state.color}
+          accessibilityLabel="Learn more about this purple button"
+        />
         </View>
       </View>
     );
