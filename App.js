0d3c8b84a3bdd00c56c7978f606d0921f82d23eb
Tim Wang <timwang@Tims-MacBook-Pro.local>
2018-05-13 15:03:43 +0200

    Move hex to own component

--- App.js
+++ App.js
@@ -11,6 +11,7 @@ export default class App extends React.Component {
   }
   
 
+
   onPressLearnMore = () => {
     this.setState({color:"#cd5422"});
   }
@@ -32,11 +33,31 @@ export default class App extends React.Component {
         >        
           <Text>X</Text>
         </TouchableOpacity>
+        <View style={styles.hexagon}>
+          <View style={styles.hexagonInner} />
+          <View style={styles.hexagonBefore} />
+          <View style={styles.hexagonAfter} />
+        </View>
+        <View style={styles.trapezoidTop} />
+        <View style={styles.trapezoidBot} />
+
+        
+        <Hex />
       </View>
     );
   }
 }
 
+export class Hex extends React.Component {
+  render() {
+    return (
+      <View> 
+        <View style={styles.trapezoidTop} />
+        <View style={styles.trapezoidBot} />        
+      </View>
+    );
+  }  
+}
 
 const styles = StyleSheet.create({
   container: {
@@ -44,5 +65,70 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+
+trapezoidTop: {
+    width: 200,
+    backgroundColor:'green',
+    borderBottomWidth: 90,
+    borderBottomColor: 'black',
+    borderLeftWidth: 40,
+    borderLeftColor: 'transparent',
+    borderRightWidth: 40,
+    borderRightColor: 'transparent',
+    borderStyle: 'solid',
+    position: 'absolute',
+    top: 100,
+  },
+  trapezoidBot: {
+    width: 200,
+    height: 0,
+    borderTopWidth: 90,
+    borderTopColor: 'red',
+    borderLeftWidth: 40,
+    borderLeftColor: 'transparent',
+    borderRightWidth: 40,
+    borderRightColor: 'transparent',
+    borderStyle: 'solid', 
+    position: 'absolute',
+    top: 190,
+  },
+
+  hexagon: {
+    width: 100,
+    height: 55,
+  },
+  hexagonInner: {
+    width: 100,
+    height: 55,
+    backgroundColor: 'blue'
+  },
+  hexagonAfter: {
+    position: 'absolute',
+    bottom: -25,
+    left: 0,
+    width: 0,
+    height: 0,
+    borderStyle: 'solid',
+    borderLeftWidth: 50,
+    borderLeftColor: 'transparent',
+    borderRightWidth: 50,
+    borderRightColor: 'transparent',
+    borderTopWidth: 25,
+    borderTopColor: 'red'
+  },
+  hexagonBefore: {
+    position: 'absolute',
+    top: -25,
+    left: 0,
+    width: 0,
+    height: 0,
+    borderStyle: 'solid',
+    borderLeftWidth: 50,
+    borderLeftColor: 'transparent',
+    borderRightWidth: 50,
+    borderRightColor: 'transparent',
+    borderBottomWidth: 25,
+    borderBottomColor: 'red'
   },
 });
